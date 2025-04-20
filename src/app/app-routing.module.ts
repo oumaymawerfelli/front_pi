ba764dac7fedaf31e2560a4780ffdcf4ad4e6293
nadazaghdoud <nada.zaghdoud@esprit.tn>
2025-04-20 23:04:16 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,70 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
-import { MarketplaceComponent } from './marketplace/marketplace.component';
-import { FrontComponent } from './component/front/front.component';
-import { BackComponent } from './component/back/back.component';
-import { ListUsersComponent } from './users/list-users/list-users.component';
-import { ProfileComponent } from './users/profile/profile.component';
-import { FormComponent } from './users/form/form.component';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './users/register/register.component';
 import { LoginComponent } from './users/login/login.component';
-import { AdminGuard } from './guards/admin.guard';
-import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { OAuth2RedirectComponent } from './users/oauth2-redirect/oauth2-redirect.component';
-import { PendingUsersComponent } from './pending-users/pending-users.component';
-import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './users/reset-password/reset-password.component';
-import { LoanApplicationComponent } from './loans/loan-application/loan-application.component';
-import { LoanInfoComponent } from './loans/loan-info/loan-info.component';
-import { LoanManagementComponent } from './loans/loan-management/loan-management.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/front', pathMatch: 'full' },
-  { path: 'marketplace', component: MarketplaceComponent },
-  {
-    path: 'front', 
-    component: FrontComponent, 
+  { path: '', component: HomeComponent },          // Default route
+  { path: 'landing', component: LandingComponent ,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'profile', component: ProfileComponent },
-      {path: 'forgot-password', component: ForgotPasswordComponent},
-      {path: 'reset-password/:token', component: ResetPasswordComponent}
-      
-    ]
-  },
-  {
-    path: 'loans', 
-    component: FrontComponent, 
-    children: [
-      { path: 'loan-management', component: LoanManagementComponent },
-      { path: 'loan-application', component: LoanApplicationComponent },
-      { path: 'loan-info', component: LoanInfoComponent },
-
- 
-    ]
-  },
-  { path: 'contact', component: ContactComponent },
-
-
-
-  { 
-    path: 'admin', 
-    component: BackComponent, 
-    canActivate: [AdminGuard],
-    children: [
-      { path: 'profile', component: ProfileComponent },
-      { path: 'users', component: ListUsersComponent, canActivate: [AdminGuard] },
-      { path: 'pending-users', component: PendingUsersComponent, canActivate: [AdminGuard]},
-    ]
-  },
-  { path: 'users', component: ListUsersComponent, canActivate: [AdminGuard] },
-  { path: 'oauth2-redirect', component: OAuth2RedirectComponent },
-  { path: 'users/new', component: FormComponent, canActivate: [AdminGuard] },
-  { path: 'unauthorized', component: UnauthorizedComponent },
-  { path: '**', redirectTo: '/front/login' },
-  
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+  ]
+} ,// /landing route
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
+import { ResetPasswordComponent } from './users/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { FormComponent } from './users/form/form.component';
+import { OAuth2RedirectComponent } from './users/oauth2-redirect/oauth2-redirect.component';
+import { ProfileComponent } from './users/profile/profile.component';
+import { PendingUsersComponent } from './pending-users/pending-users.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UsersModule } from './users/users.module';
+import { AuthService } from './core/services/auth.service';
+import { AdminGuard } from './guards/admin.guard';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { PanierComponent } from './panier/panier.component';
 import { FrontComponent } from './component/front/front.component';
 import { BackComponent } from './component/back/back.component';
-import { UsersModule } from './users/users.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { AuthService } from './core/services/auth.service';
-import { AdminGuard } from './guards/admin.guard';
-import { PendingUsersComponent } from './pending-users/pending-users.component';
+
 import { ContactComponent } from './contact/contact.component';
 import { LoanApplicationComponent } from './loans/loan-application/loan-application.component';
 import { LoanInfoComponent } from './loans/loan-info/loan-info.component';
 import { LoanManagementComponent } from './loans/loan-management/loan-management.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingComponent,
+    HomeComponent,
+    PendingUsersComponent, // only components that aren't in UsersModule
     MarketplaceComponent,
     PanierComponent,
     FrontComponent,
@@ -36,12 +51,18 @@ import { LoanManagementComponent } from './loans/loan-management/loan-management
   imports: [
     BrowserModule,
     AppRoutingModule,
+    UsersModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
     NgbModule,
     UsersModule,
     FormsModule,
     ReactiveFormsModule
   ],
   providers: [AuthService, AdminGuard],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
