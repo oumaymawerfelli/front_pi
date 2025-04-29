7eb3dde416e86eb11522fb25125fe8140431a822
nadazaghdoud <nada.zaghdoud@esprit.tn>
2025-04-29 01:32:24 +0100

    nada+oumayma+yasmine

---
+++ src/app/guards/student.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.getUserRole() === 'ROLE_STUDENT') {
+      return true;
+    } else {
+      this.router.navigate(['/unauthorized']); 
+      return false;
+    }
+  }
+}
